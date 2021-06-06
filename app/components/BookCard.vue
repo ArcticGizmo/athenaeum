4a72945813f3e6710810f531bec85f4575ba21e1
ArcticGizmo <jon.howell314@gmail.com>
2021-06-06 12:55:50 +0800

    book card

---
+++ app/components/BookCard.vue
@@ -0,0 +1,74 @@
+<template>
+  <GridLayout class="book-card" rows="* * *" columns="100 *">
+    <!-- status -->
+    <CLabel v-if="status" row="0" col="0" :text="status" textOrientation="right" />
+
+    <!-- image spot -->
+    <CLabel
+      :row="status ? 1 : 0"
+      rowSpan="3"
+      col="0"
+      class="nt-icon fas image"
+      :text="String.fromCharCode(bookIcon)"
+    />
+
+    <!-- title -->
+    <CLabel row="0" col="1" class="title" :text="book.title" />
+
+    <!-- author (S) -->
+    <CLabel row="1" col="1" class="authors" :text="book.authors.join(', ') || '--'" />
+
+    <!-- rating -->
+    <RatingField row="2" col="1" :value="3" />
+  </GridLayout>
+</template>
+
+<script>
+import RatingField from './form/RatingField.vue';
+const BOOK_ICON = 0xf02d;
+
+export default {
+  name: 'BookCard',
+  components: {
+    RatingField,
+  },
+  props: {
+    book: { type: Object, default: () => ({}) },
+  },
+  data: () => {
+    return {
+      bookIcon: BOOK_ICON,
+    };
+  },
+  computed: {
+    status() {
+      return null;
+    },
+  },
+  mounted() {
+    console.dir(this.book);
+  },
+};
+</script>
+
+<style>
+.book-card {
+  height: 130;
+  border-width: 1;
+  border-bottom-color: black;
+  padding: 10 0;
+}
+
+.book-card .image {
+  border-right-width: 1;
+  border-right-color: black;
+}
+
+.book-card .title {
+  font-size: 20;
+}
+
+.book-card .authors {
+  font-size: 20;
+}
+</style>
--- app/components/pages/Home.vue
+++ app/components/pages/Home.vue
@@ -12,9 +12,8 @@
     <ScrollView orientation="vertical">
       <!-- may want to use a list view here instead -->
       <StackLayout>
-        <Label v-for="(book, index) in books" :key="index" :text="book.title" />
+        <BookCard v-for="(book, index) in books" :key="index" :book="book" @tap.native="onTap()" />
       </StackLayout>
-      <Button text="Modal" @tap="openModal()" />
     </ScrollView>
     <!-- <GridLayout class="page__content">
       <Label class="page__content-icon fas" text.decode="&#xf015;" />
@@ -26,23 +25,28 @@
 <script>
 import * as utils from '@/shared/utils';
 import { SelectedPageService } from '@/shared/selected-page-service';
+import BookCard from '../BookCard.vue';
 import ConfirmModal from '../modals/ConfirmModal.vue';
 
 export default {
-  mounted() {
-    SelectedPageService.getInstance().updateSelectedPage('Home');
+  components: {
+    BookCard,
   },
   computed: {
     books() {
       return this.$store.books;
     },
   },
+  mounted() {
+    SelectedPageService.getInstance().updateSelectedPage('Home');
+  },
   methods: {
     onDrawerButtonTap() {
       utils.showDrawer();
     },
-    openModal() {
-      this.$modalBus.open(ConfirmModal, { message: 'Message' });
+    onTap() {
+      console.dir('--- apple')
+      this.$modalBus.open(ConfirmModal, { message: 'egg' });
     },
   },
 };
