76f042c46c61994b5d8eb18fc4692d198e399171
ArcticGizmo <jon.howell314@gmail.com>
2021-06-25 22:28:57 +0800

    Added focus and event propagation

--- app/components/Search.vue
+++ app/components/Search.vue
@@ -2,11 +2,15 @@
   <GridLayout class="search" columns="* 40">
     <!-- input -->
     <TextField
+      ref="search"
       colSpan="2"
       :value="value"
       returnKeyType="done"
       :hint="hint"
       @textChange="onTextChange"
+      @returnPress="propagate('returnPress', $event)"
+      @blur="propagate('blur', $event)"
+      @focus="propagate('focus', $event)"
     />
     <!-- search icon -->
     <CLabel class="cross" col="1" text="x" />
@@ -21,8 +25,14 @@ export default {
     hint: { type: String, default: 'Search ...' },
   },
   methods: {
+    propagate(topic, event) {
+      this.$emit(topic, event);
+    },
     onTextChange(event) {
       this.$emit('input', event.value);
+    },
+    focus() {
+      this.$refs['search'].nativeView.focus();
     },
   },
 };
